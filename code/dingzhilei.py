b97808eed2ede88450da0aa1c7f407e40af8c6d9
Blue <j5088794@gmail.com>
2014-12-01 20:12:52 +0800

    learn dingzhilei2 141201

--- code/dingzhilei.py
+++ code/dingzhilei.py
@@ -25,15 +25,8 @@ class Chain2(object):
         #返回一个完整的字符串作为新的Chain2的输入
         return Chain2('%s/%s' % (self._path, path))
 
-    #返回用户看到的字符串  print
     def __str__(self):
         return self._path
-
-    #返回开发者看到的字符串
-    def __repr__(self):
-        return self._path
-    #因通常与__str__代码保持一致, 可以简写为
-    #__repr__ = __str__
 
     def __call__(self, user):
         print 'self._path:{0}\tuser:{1}'.format(self._path, user)
