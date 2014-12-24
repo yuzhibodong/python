745a59060edbf051b7f66fe162d8930a68b92123
Blue <j5088794@gmail.com>
2014-12-24 17:05:04 +0800

    learn Pool 141224

--- code/cmd.py
+++ code/cmd.py
@@ -595,10 +595,13 @@ def long_time_task(name):
 
 if __name__ == '__main__':
     print 'Parent process %s' % os.getpid()
+    #Pool默认大小CPU的核数
+    #p = Pool(5) 可修改
     p = Pool()
     for i in range(5):
         p.apply_async(long_time_task, args=(i,))
     print 'Waiting for all subprocess done...'
     p.close()
+    #对Pool对象调用join()方法会等待所有子进程执行完毕，调用join()之前必须先调用close()，调用close()之后就不能继续添加新的Process了
     p.join()
     print 'All subprocesses done.'
--- code/test.py
+++ code/test.py
@@ -1,7 +1,1 @@
-# -*- coding:utf-8 -*-
-def prod(*s):
-    return reduce(lambda x,y:x+y,s)
-
-l = [1,2,3,4]
-
-print prod(l)
+from multiprocessing import Process
