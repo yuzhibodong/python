0a1d446f268b0af7ce76f187387ed8a0b3a5b7f5
Blue <j5088794@gmail.com>
2015-01-01 00:32:25 +0800

    learn regular expression

--- code/test.py
+++ code/test.py
@@ -1,20 +1,5 @@
 # -*- coding:utf-8 -*-
-import threading
+import re
 
-#创建全局ThreadLocal对象:
-local_school = threading.local()
-
-def process_student():
-    print 'Hello, %s (in %s)' % (local_school.student, threading.current_thread().name)
-
-def process_thread(name):
-    #绑定ThreadLocal的student
-    local_school.student = name
-    process_student()
-
-t1 = threading.Thread(target=process_thread, args=('Alice',), name='Thread-A')
-t2 = threading.Thread(target=process_thread, args=('Bob',), name='Thread-B')
-t1.start()
-t2.start()
-t1.join()
-t2.join()
+l = re.split(r'[\s\,\;]+', 'a,b;; c  d')
+print l
