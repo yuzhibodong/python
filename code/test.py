caf6bef927b003a77bc5643ea3f41288d72e2c0c
Bluethon <j5088794@gmail.com>
2015-01-21 22:31:22 +0800

    learn python 150121

--- code/test.py
+++ code/test.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import hashlib
-import h
 
-水电费
+def calc_md5(password):
+    md5 = hashlib.md5()
+    md5.update(password)
+    return md5.hexdigest()
 
-md5 = hashlib.md5()
-def sdf():
-    dffff
-    if :
-        pass
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+def login(user, password):
+    calc_md5(password)
+
+md5.
