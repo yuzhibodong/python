cb9636c3934dbe474c5bb097c8aa215dbe271aae
Bluethon <j5088794@gmail.com>
2015-02-02 22:48:08 +0800

    learn hashlib 20150202

--- code/test.py
+++ code/test.py
@@ -2,18 +2,25 @@
 # -*- coding:utf-8 -*-
 import hashlib
 
-def calc_md5(password):
+def get_md5(password):
     md5 = hashlib.md5()
     md5.update(password)
     return md5.hexdigest()
 
-db = {
-    'michael': 'e10adc3949ba59abbe56e057f20f883e',
-    'bob': '878ef96e86145580c38c87f0410ad153',
-    'alice': '99b1c2188db85afee403b1536010c2c9'
-}
+db = {}
 
-def login(user, password):
-    calc_md5(password)
+def register(username, password):
+    db[username] = get_md5(password + username + 'the-Salt')
 
-md5.
+def login(username, password):
+    pwd_md5 = get_md5(password + username + 'the-Salt')
+    if db[username] == pwd_md5:
+        print True
+    else:
+        print False
+
+username = raw_input('input your name:')
+password = raw_input('input your password:')
+
+register(username, password)
+login(username, password)
