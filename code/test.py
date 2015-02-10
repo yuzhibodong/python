2ed3ff7c67cf9e6699fc35ea6545c1c69b7b48f3
Bluethon <j5088794@gmail.com>
2015-02-10 22:44:46 +0800

    learn TCP coding 20150210

--- code/test.py
+++ code/test.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from Tkinter import *
-import tkMessageBox
+import socket
 
-class Application(Frame):
-    """docstring for Application"""
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+# AF_INET指定使用IPv4协议, AF_INET6->IPv6
+# SOCK_STREAM指定使用面向流的TCP协议
+# 创建一个socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接 参数是一个tuple
+s.connect(('www.sina.com.cn', 80))
 
-    def createWidgets(self):
-        self.helloLabel = Label(self, text='Hello, world!')
-        self.helloLabel.pack()
-        self.nameInput = Entry(self)
-        self.nameInput.pack()
-        self.quitButton = Button(self, text='Hello', command=self.hello)
-        self.quitButton.pack()
+# 发送数据:
+s.send('GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnetion: close\r\n\r\n')
 
-    def hello(self):
-        name = self.nameInput.get() or 'world'
-        tkMessageBox.showinfo('Message', 'Hello, %s' % name)
+# 接收数据:
+buffer = []
+while True:
+    # 每次最多接收1k字节:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = ''.join(buffer)
 
-app = Application()
+# 关闭连接:
+s.close()
 
-app.master.title('Hello World')
-
-app.mainloop()
+header, html = data.split('\r\n\r\n', 1)
+print header
+# 把接收的数据写入文件:
+with open('sina.html', 'wb') as f:
+    f.write(html)
