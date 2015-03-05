74b08f0cda5587185ba6844c45c0ecbbad2459b1
Bluethon <j5088794@gmail.com>
2015-03-05 23:18:16 +0800

    learn socket 20150305

--- pythoncmd.py
+++ pythoncmd.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#类执行顺序
+#从上向下执行, 定义需要放前面
+#或者采取如下格式, 就可以把代码放最上面了
+def main():
+    ...
+def other_functions():
+    ...
+if __name__ == '__main__':
+    main()
+
 #---------------------
 #字符串
 #---------------------
@@ -916,3 +926,44 @@ class Application(Frame):
 app = Application()
 app.master.title('Hello World')
 app.mainloop()
+
+#------------------------------------------------------
+#TCP编程
+#------------------------------------------------------
+#另见code/socket/
+
+#socket访问新浪首页
+
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import socket
+
+# AF_INET指定使用IPv4协议, AF_INET6->IPv6
+# SOCK_STREAM指定使用面向流的TCP协议
+# 创建一个socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接 参数是一个tuple
+s.connect(('www.sina.com.cn', 80))
+
+# 发送数据:
+s.send('GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnetion: close\r\n\r\n')
+
+# 接收数据:
+buffer = []
+while True:
+    # 每次最多接收1k字节:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = ''.join(buffer)
+
+# 关闭连接:
+s.close()
+
+header, html = data.split('\r\n\r\n', 1)
+print header
+# 把接收的数据写入文件:
+with open('sina.html', 'wb') as f:
+    f.write(html)
