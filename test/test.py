f23993e4eed04cf45930f55a1e581c20102f7fcd
Blue <j5088794@gmail.com>
2015-03-06 19:58:30 +0800

    learn email SMTP 20150306

--- test/test.py
+++ test/test.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import socket
 
-# AF_INET指定使用IPv4协议, AF_INET6->IPv6
-# SOCK_STREAM指定使用面向流的TCP协议
-# 创建一个socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 建立连接 参数是一个tuple
-s.connect(('www.sina.com.cn', 80))
+from email.mime.text import MIMEText
 
-# 发送数据:
-s.send('GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnetion: close\r\n\r\n')
+msg = MIMEText('hello, send by Python', _subtype='plain', _charset='utf-8')
 
-# 接收数据:
-buffer = []
-while True:
-    # 每次最多接收1k字节:
-    d = s.recv(1024)
-    if d:
-        buffer.append(d)
-    else:
-        break
-data = ''.join(buffer)
+from_addr = 'j5088794@163.com'
+password = 'jhb5088794'
 
-# 关闭连接:
-s.close()
+smtp_server = 'smtp.163.com'
 
-header, html = data.split('\r\n\r\n', 1)
-print header
-# 把接收的数据写入文件:
-with open('sina.html', 'wb') as f:
-    f.write(html)
+to_addr = '357892250@qq.com'
+
+import smtplib
+
+server = smtplib.SMTP(host=smtp_server, port=25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, to_addr, msg.as_string())
+server.quit()
