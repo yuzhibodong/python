26510c31d4394ef8b6b3b50275227bae4a8763a7
Blue <j5088794@gmail.com>
2015-04-08 16:58:02 +0800

    update test.py 20150408

--- test/test.py
+++ test/test.py
@@ -3,12 +3,12 @@
 
 import mysql.connector
 
-conn = mysql.connector.connect(user='root', password='5088794', database='test', use_unicode=True)
+conn = mysql.connector.connect(user='root', password='', database='test', use_unicode=True)
 
 cursor = conn.cursor()
 
 cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
-cursor.execute('insert into user (idn name) values (%s, %s)', ['1', 'Michael'])
+cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Michael'])
 cursor.rowcount
 
 conn.commit()
