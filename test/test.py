8fbee0293d26724c695782a2de992c0f5cbef4c7
bluethon <j5088794@gmail.com>
2015-05-06 23:10:17 +0800

    learn flask 20150506

--- test/test.py
+++ test/test.py
@@ -1,47 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from flask import Flask
+from flask import request
 
-# 导入:
-from sqlalchemy import Column, String, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+app = Flask(__name__)
 
-# 创建对象的基类:
-Base = declarative_base()
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
 
-# 定义User对象:
-class User(Base):
-    """docstring for User"""
-    # 表的名字:
-    __tablename__ = 'user'
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="passowrd"></p>'''
 
-    # 表的结构:
-    id = Column(String(20), primary_key=True)
-    name = Column(String(20))
-
-# 初始化数据库连接:
-# SQLAlchemy用字符串表示连接信息
-# '数据库类型+数据库驱动名称://用户名:口令@机器地址:端口号/数据库名
-engine = create_engine('mysql+mysqlconnector://root:5088794@localhost:3306/test')
-# 创建DBSession类型:
-DBSession = sessionmaker(bind=engine)
-
-# # 创建session对象:
-# session = DBSession()
-# # 创建新User对象:
-# new_user = User(id='5', name='Bob')
-# # 添加到session
-# session.add(new_user)
-# # 提交即报错到数据库:
-# session.commit()
-# # 关闭session
-# session.close()
-
-session = DBSession()
-# 创建Query查询, filter是where条件, 最后调用one()返回唯一行, 如果调用all()则返回所有行:
-user = session.query(User).filter(User.id=='5').one()
-# 打印类型和对象的name属性:
-print 'type:', type(user)
-print 'name:', user.name
-# 关闭Session
-session.close()
+<p></p>
