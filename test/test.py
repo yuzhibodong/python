09de0ad658d8c9b4fcf8cc0be1e6cf94bc83294e
bluethon <j5088794@gmail.com>
2015-05-08 16:59:56 +0800

    learn/test coroutine 20150508

--- test/test.py
+++ test/test.py
@@ -1,24 +1,25 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-from flask import Flask, request, render_template
+import time
 
-app = Flask(__name__)
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        time.sleep(1)
+        r = '200 OK'
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+def produce(c):
+    c.next()
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
 
-@app.route('/signin', methods=['GET'])
-def signin_form():
-    return render_template('form.html')
-
-@app.route('/signin', methods=['POST'])
-def signin():
-    username = request.form['username']
-    password = request.form['password']
-    if username=='admin' and password=='password':
-        return render_template('signin-ok.html', username=username)
-    return render_template('form.html', message='Bad username or password', username=username)
-
-if __name__ == '__main__':
-  app.run(debug=True)
+if __name__=='__main__':
+    c = consumer()
+    produce(c)
