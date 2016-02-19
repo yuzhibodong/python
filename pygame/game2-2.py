b0f4756ccbfc2b4c0460833026b6e25ccaadc194
bluethon <j5088794@gmail.com>
2016-02-19 16:51:34 +0800

    20160219 pygame game2-2

---
+++ pygame/game2-2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480), 0, 32)
+background_image_filename = 'sushiplate.jpg'
+background = pygame.image.load(background_image_filename).convert()
+
+x, y = 0, 0
+move_x, move_y = 0, 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+        if event.type == KEYDOWN:
+            # 键盘按下
+            if event.key == K_LEFT:
+                #按下的是左方向键的话, 把x坐标减一
+                mo
