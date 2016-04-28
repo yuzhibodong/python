5fce8ba030105199394c05fb72b27836af83cad7
bluethon <j5088794@gmail.com>
2016-04-29 00:22:06 +0800

    20160428 flask logout

--- flask/flasky/app/auth/views.py
+++ flask/flasky/app/auth/views.py
@@ -6,7 +6,7 @@
 
 
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user
+from flask.ext.login import login_user, logout_user, login_required
 
 from . import auth
 from ..models import User
@@ -16,10 +16,23 @@ from .forms import LoginForm
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # 验证表单, 然后尝试登入
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
+            # 要登录的用户 及 记住我 的值
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            # next中为 之前访问的未授权的URL, 存在request.args字典中
+            return redirect(request.args.get('next') or url_for('main1.index'))
+        # 输入有误, 提示如下Flash信息, 跳出if后再次渲染表单
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    # 调用Flask-Login的函数, 删除并重设会话
+    logout_user()
+    flash('You have benn logged out.')
+    return redirect(url_for('main1.index'))
