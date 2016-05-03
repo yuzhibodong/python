404f67cf0014802e5bb99b5b2c0eaf25040d2d30
bluethon <j5088794@gmail.com>
2016-05-03 23:12:45 +0800

    20160503 flask chapter8 token

--- flask/flasky/app/models.py
+++ flask/flasky/app/models.py
@@ -7,7 +7,10 @@
 
 # 使用Werkzeug中security模块实现 密码散列
 from werkzeug.security import generate_password_hash, check_password_hash
+# 令牌
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 # 用于登陆
+from flask import current_app
 from flask.ext.login import UserMixin
 
 from . import db
@@ -32,12 +35,15 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    # 是否邮件激活
+    confirmed = db.Column(db.Boolean, default=False)
 
+    # 只读属性, 密码散列, 原密码不存在, 返回错误
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
 
-    # 设置密码
+    # 只写属性, 设置密码
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -45,6 +51,25 @@ class User(UserMixin, db.Model):
     # 校验密码的hash, 返回True即密码正确
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # 生成token, 默认有效1h
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        # 用当然用户ID生成token
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        # 检查ID是否已存在db中, token被破, 也无法确认他人账户
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
--- flask/flasky/tests/test_user_model.py
+++ flask/flasky/tests/test_user_model.py
@@ -7,14 +7,31 @@
 
 import unittest
 
+from app import create_app, db
 from app.models import User
 
 
 class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        # 使用测试配置创建程序
+        self.app = create_app('testing')
+        # 激活上下文, 确保能在测试中使用current_app
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    # u.username为空
+    # 测试pw只写方法
     def test_password_setter(self):
         u = User(password='cat')
         self.assertTrue(u.password_hash is not None)
 
+    # 测试pw只读方法, 使用u.password返回AttributeError
     def test_no_password_getter(self):
         u = User(password='cat')
         with self.assertRaises(AttributeError):
@@ -29,3 +46,6 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_confirmed(self):
+        pass
