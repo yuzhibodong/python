ab00db7655dae36386dd0964a9b2a0fb8ffd1b2a
bluethon <j5088794@gmail.com>
2016-05-29 09:05:43 +0800

    chapter10.3 edit_profile finished

--- flask/flasky/app/auth/forms.py
+++ flask/flasky/app/auth/forms.py
@@ -5,9 +5,9 @@
 # @Link    : http://github.com/bluethon
 
 
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 
 from ..models import User
@@ -15,24 +15,24 @@ from ..models import User
 
 class LoginForm(Form):
     email = StringField(
-        'Email', validators=[Required(), Length(1, 64), Email()])
-    password = PasswordField('Password', validators=[Required()])
+        'Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
 
 
 class RegistrationForm(Form):
     email = StringField(
-        'Email', validators=[Required(), Length(1, 64), Email()])
-    # Regexp后两个参数: 旗标 和 验证失败显示的错误消息
+        'Email', validators=[DataRequired(), Length(1, 64), Email()])
+    # Regexp后两个参数: 标志位(正则选项, 如忽略大小写) 和 验证失败显示的错误消息
     username = StringField('Username', validators=[
-        Required(), Length(1, 64), Regexp(
+        DataRequired(), Length(1, 64), Regexp(
             '^[A-Za-z][A-Za-z0-9_.]*$', 0,
             'Usernames must have only letters, numbers, dots or underscores')])
     # EqualTo验证器中, 另一个字段pass2作为参数传入
     password = PasswordField('Password', validators=[
-        Required(), EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Confirm password', validators=[Required()])
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
     def validate_email(self, field):
@@ -47,27 +47,28 @@ class RegistrationForm(Form):
 
 # 修改密码
 class ChangePasswordForm(Form):
-    old_password = PasswordField('Old password', validators=[Required()])
+    old_password = PasswordField('Old password', validators=[DataRequired()])
     password = PasswordField('New password', validators=[
-        Required(), EqualTo('password2', message='Passwords must patch.')])
-    password2 = PasswordField('Confirm new password', validators=[Required()])
+        DataRequired(), EqualTo('password2', message='Passwords must patch.')])
+    password2 = PasswordField('Confirm new password',
+                              validators=[DataRequired()])
     submit = SubmitField('Update Password')
 
 
 # 申请重置密码
 class PasswordResetRequestForm(Form):
     email = StringField(
-        'Email', validators=[Required(), Length(1, 64), Email()])
+        'Email', validators=[DataRequired(), Length(1, 64), Email()])
     submit = SubmitField('Reset Password')
 
 
 # 重置密码
 class PasswordResetForm(Form):
     email = StringField(
-        'Email', validators=[Required(), Length(1, 64), Email()])
+        'Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('New Password', validators=[
-        Required(), EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Confirm Password', validators=[Required()])
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Reset Password')
 
     def validate_email(self, field):
@@ -78,8 +79,8 @@ class PasswordResetForm(Form):
 # 修改注册邮箱
 class ChangeEmailForm(Form):
     email = StringField(
-        'New Email', validators=[Required(), Length(1, 64), Email()])
-    password = PasswordField('Password', validators=[Required()])
+        'New Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Update Email Address')
 
     def validate_email(self, field):
--- flask/flasky/app/main/views.py
+++ flask/flasky/app/main/views.py
@@ -5,10 +5,14 @@
 # @Link    : http://github.com/bluethon
 
 
-from flask import render_template, abort
+from flask import render_template, redirect, url_for, abort, flash
+from flask_login import login_required, current_user
 
 from . import main
-from ..models import User
+from .forms import EditPorfileForm, EditPorfileAdminForm
+from .. import db
+from ..models import Role, User
+from ..decorators import admin_required
 
 
 @main.route('/')
@@ -22,3 +26,49 @@ def user(username):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    """ 用户编辑自己的个人信息 """
+    form = EditPorfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your Profile has been updated.')
+        return redirect(url_for('.user', username=current_user.name))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    """ 管理员编辑用户的个人信息 """
+    user = User.query.get_or_404(id)
+    form = EditPorfileAdminForm(user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('Your Profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
